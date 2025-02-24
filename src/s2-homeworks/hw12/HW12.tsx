d86f28dc0be37090fb481a1fe503098add86b3bf
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2025-02-24 10:55:57 +0300

    hw12 - change theme fix

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -1,4 +1,4 @@
-import React, {type ChangeEvent, useEffect} from 'react'
+import React, { useEffect} from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
